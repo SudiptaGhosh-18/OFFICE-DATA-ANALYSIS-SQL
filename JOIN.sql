114e29d7bae747afe013ad6f617c00b9bde5ebba
SUDIPTA GHOSH <oishi5591@gmail.com>
2026-05-02 22:49:15 +0530

    Add department table and join query to JOIN.sql

---
+++ JOIN.sql
@@ -0,0 +1,19 @@
+create table employee(
+  emp_id int primary key,
+  emp_name varchar(50),
+  salary numeric(10,2),
+  dept_id numeric(10),
+  manager_id numeric(10)
+  );
+select * from employee;
+
+create table department(
+  dept_id int primary key,
+  dept_name varchar(50)
+  );
+select * from department;
+
+select e.emp_name,d.dept_name 
+  from employee e 
+inner join department d on
+e.dept_id=d.dept_id;
