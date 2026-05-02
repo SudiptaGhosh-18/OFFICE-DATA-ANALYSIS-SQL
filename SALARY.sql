89393fd3700158d94c220e511681abe156ab553c
SUDIPTA GHOSH <oishi5591@gmail.com>
2026-05-02 22:27:03 +0530

    Create employee and department tables with queries

--- SALARY.sql
+++ SALARY.sql
@@ -1,1 +1,16 @@
-
+create table employee(
+  emp_id int primary key,
+  emp_name varchar(50),
+  salary numeric(10,2),
+  dept_id numeric(10),
+  manager_id numeric(10)
+  );
+select * from employee;
+create table department(
+  dept_id int primary key,
+  dept_name varchar(50)
+  );
+select * from department;
+select emp_name ,salary 
+  from employee
+where salary>50,000;
